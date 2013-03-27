0e82d9cee328d04e93e8428a7bdcf0cee0adb45b
fabianschiller <fabian.schiller@gmail.com>
2013-03-27 20:57:14 +0100

    new layout and icon-set

--- ThatsMyKid/assets/www/js/init.js
+++ ThatsMyKid/assets/www/js/init.js
@@ -1,15 +1,15 @@
 THATSMYKID.deviceready = function() {
-	THATSMYKID.initialHeight = window.innerHeight / 2;
-	THATSMYKID.initialWidth = window.innerWidth / 4;
-	
-	var fCanvas = document.getElementById("canvas1");
-	
-	fCanvas.style.width = THATSMYKID.initialWidth + "px";
-	fCanvas.style.height = THATSMYKID.initialHeight + "px";
+//	THATSMYKID.initialHeight = window.innerHeight / 2;
+//	THATSMYKID.initialWidth = window.innerWidth / 4;
+//	
+//	var fCanvas = document.getElementById("canvas1");
+//	
+//	fCanvas.style.width = THATSMYKID.initialWidth + "px";
+//	fCanvas.style.height = THATSMYKID.initialHeight + "px";
 	// add touchlistener:
-	fCanvas.addEventListener('touchmove', THATSMYKID.listener.touchListen, false);
+//	fCanvas.addEventListener('touchmove', THATSMYKID.listener.touchListen, false);
 
-	var preferencesButton = document.getElementById("preferencesButton");
+	var preferencesButton = document.getElementById("emptyPicture");
 	preferencesButton.addEventListener('touchstart', THATSMYKID.listener.preferencesButton, false);
 	
 	var galleryButton = document.getElementById("galleryButton");
