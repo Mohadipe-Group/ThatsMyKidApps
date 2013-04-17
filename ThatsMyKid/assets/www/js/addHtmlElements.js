763a911261357247e7211019f7d29403f426bc0f
fabianschiller <fabian.schiller@gmail.com>
2013-04-17 21:23:01 +0200

    simplified inserting pictures to app body

--- ThatsMyKid/assets/www/js/addHtmlElements.js
+++ ThatsMyKid/assets/www/js/addHtmlElements.js
@@ -1,6 +1,5 @@
 function addElement(url) {
-  removeEmptyPicture();
-  var ni = document.getElementById('stage');
+  var appBody = document.getElementById('stage');
   
   var divClassName = 'picture';
   
@@ -17,44 +16,38 @@ function addElement(url) {
   var divStyleContent = 'background-image:url(DSC_0437.JPG);background-size: Auto 100%;background-position:center;';
   newdiv.setAttribute('style', divStyleContent);
 
-  ni.appendChild(newdiv);
-  addEmptyPicture();
+  appBody.insertBefore(newdiv, document.getElementById('emptyPicture'));
   
   ersetzeBackgroundImage(newdiv, url);
+  
+  addTouchListeners(newdiv);
   
   return divIdName;
 }
 
-function removeEmptyPicture() {
-  var ni = document.getElementById('stage');
-  var emptydiv = document.getElementById('emptyPicture');
-  ni.removeChild(emptydiv);
+function addTouchListeners(newdiv) {
+	var touched = false;
+	newdiv.addEventListener('click', function(e)
+	{
+	    touched = false;
+	});
+	newdiv.addEventListener('touchstart', function(e){
+	    touched = true;
+	    setTimeout(function(){
+	        if (touched)
+	        	THATSMYKID.listener.pictureEditButton(e);
+	    },1000);
+	});
+	newdiv.addEventListener('touchmove', function(e){
+	    touched = false;
+	});
+	newdiv.addEventListener('touchend', function(e){
+	    touched = false;
+	});
 }
 
-function addEmptyPicture() {
-	var ni = document.getElementById('stage');
-
-	var emptydiv = document.createElement('div');
-	emptydiv.setAttribute('class', 'picture');
-	emptydiv.setAttribute('id', 'emptyPicture');
-
-	var img = document.createElement('img');
-	
-	img.setAttribute('id', 'fingerPicture');
-	img.setAttribute('width', '60%');
-	img.setAttribute('src', 'res/finger.png');
-	
-	emptydiv.appendChild(img);
-	ni.appendChild(emptydiv);
-	reInitPreferencesButton();
-}
 
 function ersetzeBackgroundImage(targetDiv, picUrl) {
 	var divStyleContent = 'background-image:url(' + picUrl + ');background-size: Auto 100%;background-position:center;';
 	targetDiv.setAttribute('style', divStyleContent);
-}
-
-function reInitPreferencesButton() {
-	var preferencesButton = document.getElementById("emptyPicture");
-	preferencesButton.addEventListener('touchstart', THATSMYKID.listener.preferencesButton, false);
 }
